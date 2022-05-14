d93eaf0792e8c5343f9b9a50113fead1924fc82e
Serine99 <serine.sardaryan1@gmail.com>
2022-05-14 21:50:33 +0400

    validate function

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
 function validate () {
-    let password = document.getElementById('password');
-    if (password.value.match(/[0-9]/))
+    const password = document.getElementById('password');
+    const upper = document.getElementById('upper');
+    const lower = document.getElementById('lower');
+    const number = document.getElementById('number');
+    const length = document.getElementById('length');
+    const char = document.getElementById('char');
+    if (password.value.match(/[0-9]/)){
+        number.style.color = 'green'
+    } else { number.style.color = 'red'};
+    if (password.value.match(/[A-Z]/)){
+        upper.style.color = 'green'
+    } else { upper.style.color = 'red'}
+
+    if (password.value.match(/[a-z]/)){
+        lower.style.color = 'green'
+    } else { lower.style.color = 'red'}
+
+    if (password.value.match(/[!\@\#\$\%\^\&\*]/)){
+        char.style.color = 'green'
+    } else { char.style.color = 'red'}
+
+    if (password.value.length >= 8 && password.value.length <=16 ){
+        length.style.color = 'green'
+    } else { length.style.color = 'red'}
+
+
 }
